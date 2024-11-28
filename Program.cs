a1d2b7688fbef48b854b097c732ffaab7c523ddb
Administrator <murillowebdesigner@gmail.com>
2024-11-28 19:45:53 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the IMC Calculator!");
+
+        Console.Write("Enter your weight (kg): ");
+        double weight = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter your height (m): ");
+        double height = double.Parse(Console.ReadLine());
+
+        double imc = CalculateIMC(weight, height);
+        Console.WriteLine($"Your IMC is: {imc:F2}");
+
+        string category = GetIMCCategory(imc);
+        Console.WriteLine($"You are classified as: {category}");
+    }
+
+    public static double CalculateIMC(double weight, double height)
+    {
+        return weight / (height * height);
+    }
+
+    public static string GetIMCCategory(double imc)
+    {
+        return imc switch
+        {
+            < 18.5 => "Underweight",
+            >= 18.5 and < 24.9 => "Normal weight",
+            >= 25 and < 29.9 => "Overweight",
+            >= 30 => "Obesity",
+            _ => "Invalid"
+        };
+    }
+}
